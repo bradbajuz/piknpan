9fdd1bd8ce552f7f182030cf4837f6e2d2a1196b
bradbajuz <brad@bradballard.com>
2014-05-15 20:15:21 -0400

    Create recipes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'recipes/index'
+
+  get 'recipes/show'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   match 'about', to: 'welcome#about', via: :get
