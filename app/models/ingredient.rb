f831a7f88ce4b001672bfa47069cc0995e904dfa
bradbajuz <brad@bradballard.com>
2014-05-20 21:34:26 -0400

    Update validation

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,6 @@ class Ingredient < ActiveRecord::Base
   has_many :item_in_pantries
 
   validates :name, length: { minimum: 2 }, presence: true
-  validates :category, length: { minimum: 3 }
+  validates :category, length: { minimum: 3 }, if: :location?
   validates :item_in_pantries, presence: true
 end
