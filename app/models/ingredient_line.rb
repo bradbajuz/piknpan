0e40557ed3d0616cae0cfb564dafb55868f13e77
bradbajuz <brad@bradballard.com>
2014-05-22 22:07:11 -0400

    Fix validation calling wrong column

--- app/models/ingredient_line.rb
+++ app/models/ingredient_line.rb
@@ -3,7 +3,7 @@ class IngredientLine < ActiveRecord::Base
   belongs_to :ingredient
 
   validates :quantity, length: { minimum: 1 }, presence: true
-  validates :directions, length: { minimum: 10 }, if: :location?
+  validates :directions, length: { minimum: 10 }, if: :directions?
   validates :recipe, presence: true
   validates :ingredient, presence: true
   validates :measurement, length: { minimum: 1 }, presence: true
