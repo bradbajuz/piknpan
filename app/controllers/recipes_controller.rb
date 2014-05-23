290b66a21d038e8caeb766644415de7eff4df912
bradbajuz <brad@bradballard.com>
2014-05-23 19:21:41 -0400

    Add logic to recipe to create relation to users pantry and recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,14 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all {|recipe| recipe.matches_pantry (current_user)}
 
-    authorize @recipes
+    # authorize @recipes
   end
 
   def show
     @recipe = Recipe.find(params[:id])
+
+    # @recipe_weight = current_user.recipe_weights.where("weight > 50").order(weight: :desc)
 
     authorize @recipe
   end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,9 +3,18 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients, through: :ingredient_lines
   has_many :directions
 
+  def matches_pantry(user)
+    count = 0
+    ingredients.each do |ingredient|
+      count += 1 if ingredient.item_in_pantries.where(user: user).size >= 1
+    end
+    (count * 100)/ingredients.length
+  end
+
   validates :name, length: { minimum: 2 }, presence: true
   validates :description, length: { minimum: 10 }, presence: true
   validates :ingredient_lines, presence: true
   validates :ingredients, presence: true
   validates :directions, presence: true
+
 end
