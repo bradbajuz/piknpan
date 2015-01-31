20d5f22809103c97ea5357bda053e7f8f6fa4556
Brad Ballard <brad@bradballard.com>
2015-01-30 22:43:31 -0500

    Allow user created ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,6 +3,6 @@ class Ingredient < ActiveRecord::Base
   has_many :ingredient_lines
   has_many :recipes, through: :ingredient_lines
 
-  validates :name, length: { minimum: 2 }, presence: true
+  validates :name, presence: true
   validates :category, length: { minimum: 3 }, if: :category?
 end
--- app/models/ingredient_line.rb
+++ app/models/ingredient_line.rb
@@ -15,7 +15,7 @@ class IngredientLine < ActiveRecord::Base
   end
 
   def ingredient_name=(name)
-    self.ingredient = Ingredient.find_by_name(name) if name.present?
+    self.ingredient = Ingredient.find_or_create_by(name: name) if name.present?
   end
 
   def calculate_matches
